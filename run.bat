3320622a97d2ed691254317139a586928cc3fcd0
Martin Cuellar <nelbren@gmail.com>
2024-08-06 23:21:19 -0600

    v0.1.3: check if module (requests) is installed

--- run.bat
+++ run.bat
@@ -3,7 +3,12 @@ IF EXIST "venv\Scripts\activate" (
   venv\Scripts\activate
   IF EXIST "my_set_env.bat" (
     call my_set_env.bat
-    venv\Scripts\python.exe grades-weighted-automatically.py
+    python -c "import requests" 2>nul
+    IF %ERRORLEVEL% == 0 (
+      venv\Scripts\python.exe grades-weighted-automatically.py
+    ) ELSE (
+      ECHO Please install the modules with: install_requirements.bat
+    )
   ) ELSE (
     ECHO Please make my_set_env.bat!
     ECHO   1^) copy my_set_env.bat.example my_set_env.bat
@@ -13,4 +18,3 @@ IF EXIST "venv\Scripts\activate" (
 ) ELSE (
   ECHO Please make my virtual environment! ( python3 -m venv venv ^)
 )
-
