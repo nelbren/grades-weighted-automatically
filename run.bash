d832235ac1bdcbf76cc15b500fcebc4f0cacdad7
Martin Cuellar <nelbren@gmail.com>
2024-08-07 22:54:10 -0600

    v0.1.7: fix 'intall.bash' -> 'install.bash' ;)

--- run.bash
+++ run.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ ! -r venv/bin/activate ]; then
-  echo "Please make my virtual environment! ( ./intall.bash )"
+  echo "Please make my virtual environment! ( ./install.bash )"
   exit 1
 fi
 
 source venv/bin/activate
 
 if [ ! -r my_set_env.bash ]; then
-  echo "Please make my_set_env.bash! ( ./intall.bash )"
+  echo "Please make my_set_env.bash! ( ./install.bash )"
   exit 2
 fi
 
